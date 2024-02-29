282449164d56c5f4545b8115a281f0db2c64564e
Tannu <thanveertannu027@gmail.com>
2024-02-29 23:15:58 +0530

    Initial commit

---
+++ src/main/java/com/example/demoForJpa/repo/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.example.demoForJpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demoForJpa.entity.Student;
+
+public interface StudentRepo extends JpaRepository<Student,Long> {
+
+	Object save(long id);
+
+}
---
+++ src/main/java/com/example/demoForJpa/repo/StudentRepo2.java
@@ -0,0 +1,5 @@
+package com.example.demoForJpa.repo;
+
+public interface StudentRepo2 {
+
+}
